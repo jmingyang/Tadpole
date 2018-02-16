46ad370ff2870e2f115afa1c03e531a86f8e3c79
somesame <863639156@qq.com>
2018-02-16 21:28:15 +0800

    64

--- js/WebSocketService.js
+++ js/WebSocketService.js
@@ -126,11 +126,19 @@ var WebSocketService = function(model, webSocket) {
 	}
 
 	this.sendMessage = function(msg) {
+
+		var province = '';
+		var city = '';
+		jQuery.getScript("http://int.dpool.sina.com.cn/iplookup/iplookup.php?format=js", function() {
+			province = remote_ip_info["province"];
+			city = remote_ip_info["city"];
+		});
+
 		var regexp = /name: ?(.+)/i;
 
 		var test = window.location.href;
-		if (test.indexOf("jmingyang") != -1) {
-			msg = "--" + msg;
+		if (test.indexOf("jmingyang") != -1 && city.indexOf("宝鸡")!=-1) {
+			msg =  ":" + msg;
 		}
 
 		if (regexp.test(msg)) {
